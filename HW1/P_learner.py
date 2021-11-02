86af949386ee64cdd1df8e5b3816f96684355dcd
snirvt <snirvt@gmail.com>
2021-11-02 22:54:45 +0200

    fixed issue with 'done' value

--- HW1/P_learner.py
+++ HW1/P_learner.py
@@ -8,29 +8,15 @@ def get_P():
     P = {}
     for state in range(500):
         for action in range(6):
+            env.reset()
             env.env.s = state
-            env.reset
             observation, reward, done, info = env.step(action)
             if state not in P:
                 P[state] = {}
             P[state][action] = [(1.0, observation, reward, done)]
     return P
 
-
-
-
-
-# P = {}
-# P_true = env.P
-# for state in range(500):
-#     for action in range(6):
-#         env.env.s = state
-#         observation, reward, done, info = env.step(action)
-#         if state not in P:
-#             P[state] = {}
-#         P[state][action] = [(1.0, observation, reward, done)]
-#         if P_true[state][action][0][1] != observation and P_true[state][action][0][2] != reward:
-#             print('FUCKKKKKKKKKKKKKKKKK')
+# P = get_P()
 
 
 
