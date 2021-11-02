0cfeb7fad1fcffc474351f294aa391af073e0fae
snirvt <snirvt@gmail.com>
2021-11-02 23:36:58 +0200

    added terminal state dict

--- HW1/P_learner.py
+++ HW1/P_learner.py
@@ -6,6 +6,7 @@ from cheats import new_reset, modify_env
 def get_P():
     env = gym.make('Taxi-v3')
     P = {}
+    T = {} # terminal states
     for state in range(500):
         for action in range(6):
             env.reset()
@@ -13,19 +14,12 @@ def get_P():
             observation, reward, done, info = env.step(action)
             if state not in P:
                 P[state] = {}
+            if done: # if terminal state
+                T[state] = []
             P[state][action] = [(1.0, observation, reward, done)]
-    return P
-
-# P = get_P()
-
-
-
-
-
-
-
-
-
+    return P, T
+# P, T = get_P()
+    
 
 
 
