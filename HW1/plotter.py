c25b9bdf818c410f89bdc74d2b3de9b7039feee9
snirvt <snirvt@gmail.com>
2021-11-07 20:39:37 +0200

    better graph

--- HW1/plotter.py
+++ HW1/plotter.py
@@ -6,9 +6,16 @@ from policy_iteration import policy_iteration
 from P_learner import get_P
 
 P, T, PT = get_P()
-policy, value, value_sum = policy_iteration(P, T, PT)
 
-plt.plot(value_sum)
+fig = plt.figure()
+
+for GAMMA in [0.95]:
+    policy, value, value_sum = policy_iteration(P, T, PT, GAMMA=GAMMA, quit_when_optimal = True)
+    plt.plot(value_sum, label=GAMMA)
+plt.legend(title="GAMMA")
+fig.suptitle('Value Function Sum Over Iterations', fontsize=20)
+plt.xlabel('Iteration Number', fontsize=15) 
+plt.ylabel('Value Function', fontsize=15)
 plt.show()
 
 
